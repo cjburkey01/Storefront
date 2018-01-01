9eec7bd9b66934d0bd5ea19b9711240570d076c0
CJ Burkey <cjburkey01@gmail.com>
2018-01-01 13:12:10 -0500

    Camera and day/night cycle

---
+++ Storefront/Assets/Script/CameraMove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public float moveSpeed = 5.0f;
+	public float rotateSpeed = 180.0f;
+	public Vector2 minMaxRotation = new Vector2(15.0f, 50.0f);
+	public Vector2 minMaxYPos = new Vector2(5.0f, 20.0f);
+
+	void Update() {
+		HandleRotation();
+		HandleMovement();
+		HandleClamping();
+	}
+
+	private void HandleRotation() {
+		if (Input.GetMouseButton(0)) {
+			Vector3 point = GetCameraPoint();
+			transform.RotateAround(point, Vector3.up, Input.GetAxisRaw("Mouse X") * rotateSpeed * Time.deltaTime);
+		}
+	}
+
+	private void HandleMovement() {
+		Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+		movement = transform.TransformDirection(movement.normalized);
+		movement.y = 0.0f;
+		movement.Normalize();
+		movement *= moveSpeed * Time.deltaTime;
+		transform.position += movement;
+	}
+
+	private void HandleClamping() {
+		Vector3 pos = transform.position;
+		pos.y = Mathf.Clamp(pos.y, minMaxYPos.x, minMaxYPos.y);
+		transform.position = pos;
+		Vector3 rotation = transform.rotation.eulerAngles;
+		rotation.x = GetRotationX();
+		transform.rotation = Quaternion.Euler(rotation);
+	}
+
+	private Vector3 GetCameraPoint() {
+		Ray ray = new Ray(transform.position, transform.forward);
+		Plane plane = new Plane(Vector3.up, Vector3.zero);
+		float distance;
+		if (plane.Raycast(ray, out distance)) {
+			return ray.GetPoint(distance);
+		}
+		return Vector3.zero;
+	}
+
+	private float GetRotationX() {
+		float perc = transform.position.y / (minMaxYPos.y - minMaxYPos.x);
+		return perc * (minMaxRotation.y - minMaxRotation.x) + minMaxRotation.x;
+	}
+
+}
---
+++ Storefront/Assets/Script/TimeHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TimeHandler : MonoBehaviour {
+
+	public float dayLength = 300.0f;
+	public float startTime = 45.0f;
+	public float sunIntensitySmoothing = 0.1f;
+
+	private float worldTime;
+
+	private Vector3 euler;
+	private float pitch;
+	private float yaw;
+	private float roll;
+	private float vel;
+
+	private Light sun;
+
+	void Start() {
+		worldTime = startTime;
+
+		pitch = transform.rotation.eulerAngles.y;
+		roll = transform.rotation.eulerAngles.z;
+
+		sun = GetComponent<Light>();
+	}
+
+	void Update() {
+		worldTime += Time.deltaTime;
+		SetSun();
+	}
+
+	private void SetSun() {
+		yaw = GetTimeOfDay() / dayLength * 360.0f;
+		transform.rotation = Quaternion.Euler(yaw, pitch, roll);
+
+		if (yaw >= 180.0f) {
+			sun.intensity = Mathf.SmoothDamp(sun.intensity, 0.0f, ref vel, sunIntensitySmoothing);
+		} else {
+			sun.intensity = Mathf.SmoothDamp(sun.intensity, 1.0f, ref vel, sunIntensitySmoothing);
+		}
+	}
+
+	public float GetTime() {
+		return worldTime;
+	}
+
+	public float GetTimeOfDay() {
+		return worldTime % dayLength;
+	}
+
+}
